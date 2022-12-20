51f3d8b786d9c885fa7bb131b638bb2ee1bb75cb
BlueTompon <lucastran00@gmail.com>
2022-12-20 20:00:55 +0100

    tweak

--- src/shaders/PostFrag.js
+++ src/shaders/PostFrag.js
@@ -21,9 +21,9 @@ void main() {
   vec4 distxs = texture(tDist, vUv/coverRatio /1. +  0.3* vec2(cos(time) , sin(time)) + 24.1);
 
   vec2 largeDisplacment = vec2(cos(time * 2.), sin(time * 2.)) *mix(0.1, 1. ,progE)* (- distl.g + distl.b)  * 0.02;
-  vec2 smallDisplacment = vec2(cos(time*2.), sin(time* 2.)) *progE * (dists.r + dists.g -4. * dists.b) * 0.005;
+  vec2 smallDisplacment = vec2(cos(time*2.), sin(time* 2.)) *progE * (dists.r + dists.g -4. * dists.b) * 0.003;
 
-  vec2 smallDisplacmentNormal = vec2(cos(time*2.), sin(time* 2.)) *mix(0.1, 1., progE) * (-2. *distxs.r ) * 0.001;
+  vec2 smallDisplacmentNormal = vec2(cos(time*2.), sin(time* 2.)) *mix(0.1, 1., progE) * (-2. *distxs.r ) * 0.0005;
   // I've tried with some normal movement but I not sure of the feeling
   // vec2 smallDisplacmentNormal = vec2(-sin(time*2.), cos(time* 2.)) *mix(0.1, 1., progE) * (-2. *distxs.r ) * 0.001;
 
