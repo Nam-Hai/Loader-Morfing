7837668b6e576fb2b83e339501c1c7c269a96f92
BlueTompon <lucastran00@gmail.com>
2022-12-20 19:54:29 +0100

    small tweak to the animation

--- src/shaders/PostFrag.js
+++ src/shaders/PostFrag.js
@@ -18,11 +18,15 @@ void main() {
   // the normal map is loaded two time with different zoom and translation
   vec4 distl = texture(tDist, vUv/coverRatio /6. -  0.1* vec2(time, time * 0.4 ));
   vec4 dists = texture(tDist, vUv/coverRatio /2. +  0.2* vec2(time * 1., -time * .8 ) + 2545.6);
-  vec4 distxs = texture(tDist, vUv/coverRatio /1. +  0.4* vec2(cos(time) , sin(time)) + 24.1);
+  vec4 distxs = texture(tDist, vUv/coverRatio /1. +  0.3* vec2(cos(time) , sin(time)) + 24.1);
 
   vec2 largeDisplacment = vec2(cos(time * 2.), sin(time * 2.)) *mix(0.1, 1. ,progE)* (- distl.g + distl.b)  * 0.02;
-  vec2 smallDisplacment = vec2(cos(time*2.), sin(time* 2.)) *progE * (dists.r + dists.g -4. * dists.b) * 0.007;
-  vec2 smallDisplacmentNormal = vec2(-sin(time*2.), cos(time* 2.)) *mix(0.0, 1., progE) * (-2. *distxs.r ) * 0.002;
+  vec2 smallDisplacment = vec2(cos(time*2.), sin(time* 2.)) *progE * (dists.r + dists.g -4. * dists.b) * 0.005;
+
+  vec2 smallDisplacmentNormal = vec2(cos(time*2.), sin(time* 2.)) *mix(0.1, 1., progE) * (-2. *distxs.r ) * 0.001;
+  // I've tried with some normal movement but I not sure of the feeling
+  // vec2 smallDisplacmentNormal = vec2(-sin(time*2.), cos(time* 2.)) *mix(0.1, 1., progE) * (-2. *distxs.r ) * 0.001;
+
 
   vec4 texture = texture(tMap,vUv -  largeDisplacment + smallDisplacment + smallDisplacmentNormal);
 
